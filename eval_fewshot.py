ecfed2fc7ae2896e41ec1473896609f1a2d510dc
Erin1101 <87416956+Erin1101@users.noreply.github.com>
2024-04-06 17:38:45 +0800

    max likelihood

--- eval_fewshot.py
+++ eval_fewshot.py
@@ -257,7 +257,7 @@ def main():
         with torch.no_grad():
             # task 6
             outputs = model(encoding["input_ids"], encoding['labels'])
-            log_likelihood = -torch.log_softmax(outputs.logits.mean(dim=1), dim=-1)
+            log_likelihood = -torch.log_softmax(outputs.logits.max(dim=1), dim=-1)
 
 
         print("Saving results to {}".format(output_file))
